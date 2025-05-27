cdde86e050d35ab96eaf58c61bed8eebf2e0ea96
ARDRYR <yyryyr5365@naver.com>
2025-05-27 21:13:48 +0900

    카드생성~

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import './App.css';
+import Card from './components/Card';
+
 export default function App() {
 
   return (
     <>
-    d
+      <Card
+        imageSource='https://media.istockphoto.com/id/507968212/ko/%EC%82%AC%EC%A7%84/%EB%B9%A8%EA%B0%84%EC%83%89-%EB%8B%A8%EA%B6%A4-%EC%95%8C%ED%8C%8C%EB%B2%B3-a.jpg?s=612x612&w=0&k=20&c=k9XmiavCnemNz6zL5Baa3o0L9FdSGfwijstKWXIiFmc='
+        title='엑스테리토리얼'
+        score={6.7}
+      />
     </>
   )
 }
-
-
---
+++ src/components/Card.tsx
@@ -0,0 +1,20 @@
+interface CardProps {
+  imageSource: string;
+  title: string;
+  score: number;
+}
+
+export default function Card({ imageSource, title, score }: CardProps) {
+
+  return(
+    <div className="card">
+      <img className="card-image" src={imageSource}/>
+      <div className="card-infomation">
+        <span className="card-title">{title}</span>
+        <span className="card-score">⭐ {score}</span>
+      </div>
+    </div>
+  );
+}
+
+
